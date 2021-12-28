db78b818f5b45ecf040381526df3f9580b9cbf88
Lance <53155359+Lancelof2019@users.noreply.github.com>
2021-12-28 17:30:08 +0100

    Create antiInverseImage.cpp

---
+++ functionspace/antiInverseImage/antiInverseImage.cpp
@@ -0,0 +1,18 @@
+
+#include "/opt/project_space/test_mnist/HPC_comb_code/MA/Watershed_Algorithm/headerspace/WatershedAlg.h"
+using namespace cv;
+
+Mat WatershedAlg::antiInverseImage(Mat image) {
+        for(int i = 0; i < image.rows; i++) {
+
+            for(int j = 0; j < image.cols; j++) {
+                if( (int) image.at<uchar>(i,j) == ZERO ) {
+                    image.at<uchar>(i,j) = ZERO;
+                } else {
+                    image.at<uchar>(i,j) = ONE;
+                }
+            }
+        }
+
+        return image;
+    }
