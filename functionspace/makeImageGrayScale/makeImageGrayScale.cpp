fdc17e7468251045139f2367095478893b24a5fd
Lance <53155359+Lancelof2019@users.noreply.github.com>
2022-03-17 11:35:20 +0100

    Update makeImageGrayScale.cpp

--- functionspace/makeImageGrayScale/makeImageGrayScale.cpp
+++ functionspace/makeImageGrayScale/makeImageGrayScale.cpp
@@ -2,37 +2,18 @@
 using namespace cv;
 
 Mat WatershedAlg::makeImageGrayScale(Mat image) {
-        Mat grayScale(image.rows, image.cols, CV_8UC1, Scalar(70));
-        
-    
 
-
-
-        for(int i = 0; i < image.rows; i++) {
-
-            for(int j = 0; j < image.cols; j++) {
-                double gray = 0.21 * image.at<cv::Vec3b>(i,j)[0] +
-                              0.72 * image.at<cv::Vec3b>(i,j)[1] +
-                              0.07 * image.at<cv::Vec3b>(i,j)[2];
-                grayScale.at<uchar>(i,j) = (uchar) gray;
+      Mat grayScale(image.rows, image.cols, CV_8UC1, Scalar::all(0));
+      for(int i = 0; i < image.rows; i++) {
+          for(int j = 0; j < image.cols; j++) {
+             double gray = 0.21 * image.at<cv::Vec3b>(i,j)[0] +
+                           0.72 * image.at<cv::Vec3b>(i,j)[1] +
+                           0.07 * image.at<cv::Vec3b>(i,j)[2];
+             grayScale.at<uchar>(i,j) = (uchar) gray;
             }
-        }
-       cv::GaussianBlur(grayScale, grayScale, Size(5,5), 1);
-        string filename8="/opt/test8.txt";
-        ofstream fout8(filename8);
-
-        for(int i = 0; i < image.rows; i++) {
-            for(int j = 0; j < image.cols; j++) {
-
-             fout8<<((int)grayScale.at<uchar>(i,j))<<",";
-             
-
-         }
-          fout8<<endl;
-     }
-     fout8.close();
-
-
-        return grayScale;
+       }       
+       cv::GaussianBlur(grayScale, grayScale, Size(3,5), 3,4);
+       image.release();
+       return grayScale;
     }
 
