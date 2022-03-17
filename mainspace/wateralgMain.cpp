48273d576fcf7e083bf4aea4c2030ea87b43df41
Lance <53155359+Lancelof2019@users.noreply.github.com>
2022-03-17 11:40:15 +0100

    Update wateralgMain.cpp

--- mainspace/wateralgMain.cpp
+++ mainspace/wateralgMain.cpp
@@ -3,23 +3,11 @@
  using namespace std;
  using namespace cv;
  int main(){
-     cout<<"---Image will be read into memory-----"<<endl;
-     cv::Mat srcImg=cv::imread("/opt/project_space/test_mnist/HPC_comb_code/watershed_demo/energy/12_energy_map.png");
-     
-     //opt/project_space/test_mnist/HPC_comb_code/watershed_demo/energy/12_energy_map.png");
-     
-     
-     ///opt/project_space/test_mnist/HPC_comb_code/watershed_demo/energy/32_energy_map.png");
-  
-     cv::Mat duplImage=srcImg;
-     ///opt/project_space/test_mnist/HPC_comb_code/watershed_demo/energy/12_energy_map.png");
-
-         
-         //"Copyright-Norfolk-Castle-Museum.jpg");  
-    
-     //opt/project_space/te st_mnist/HPC_comb_code/watershed_demo/energy/2_energy_map.png");
-     cv::imshow("test1",srcImg);
-     cv::waitKey();
+     cv::Mat srcImg=cv::imread("/opt/testimages/istockphoto-1150481340-612x612.jpg");
+     cv::imshow("Test image",srcImg);
+     cv::waitKey(0);
+     cv::Mat duplImage(srcImg.rows,srcImg.cols,CV_8UC3,Scalar::all(0));
+     srcImg.copyTo(duplImage);
      WatershedAlg *walg=new WatershedAlg();
      walg->processImage(srcImg,duplImage);
-   }
+   } 
