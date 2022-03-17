dc70fe418eadd05dd27f5061cb7a9be1febaacdb
Lance <53155359+Lancelof2019@users.noreply.github.com>
2022-03-17 11:35:48 +0100

    Update erosion.cpp

--- functionspace/erosion/erosion.cpp
+++ functionspace/erosion/erosion.cpp
@@ -2,19 +2,29 @@
 using namespace cv;
 
 Mat WatershedAlg::erosion(Mat image, vector< vector <int> > kernel) {
-        int n = kernel.size();
-        int m = kernel[0].size();
 
-        vector< vector < bool> > shouldBeZeroImage(image.rows, vector<bool>(image.cols, false));
-        for(int i = n / 2; i < image.rows - n / 2; i++) {
-            for(int j = m / 2; j < image.cols - m / 2; j++) {
+
+    int morph_size = 2;
+    Mat element = getStructuringElement(cv::MORPH_RECT, Size(2 * morph_size + 1,2 * morph_size + 1),
+    Point(morph_size, morph_size));
+    
+    Mat dill(image.rows,image.cols,CV_8UC1,Scalar::all(0));
+
+    dilate(image, dill, element,Point(-1, -1), 1);
+
+    int n = kernel.size();
+    int m = kernel[0].size();
+    image.release();
+    vector< vector < bool> > shouldBeZeroImage(dill.rows, vector<bool>(dill.cols, false));
+        for(int i = n / 2; i < dill.rows - n / 2; i++) {
+            for(int j = m / 2; j < dill.cols - m / 2; j++) {
 
                 // Loop the kernel
-                if( (int) image.at<uchar>(i, j) == ONE) {
+                if( (int) dill.at<uchar>(i, j) == ONE) {
                     bool shouldBeZero = false;
                     for(int crtX = i - n / 2, x = 0; crtX <= i + n / 2; crtX++, x++) {
                         for(int crtY = j - m / 2, y = 0; crtY <= j + m / 2; crtY++, y++) {
-                            if((int) image.at<uchar>(crtX, crtY) == ZERO && kernel[x][y] == 1) {
+                            if((int) dill.at<uchar>(crtX, crtY) == ZERO && kernel[x][y] == 1) {
                                 shouldBeZero = true;
                                 break;
                             }
@@ -28,29 +38,13 @@ Mat WatershedAlg::erosion(Mat image, vector< vector <int> > kernel) {
             }
         }
 
-        for(int i = 0; i < image.rows; i++) {
-            for(int j = 0; j < image.cols; j++) {
+        for(int i = 0; i < dill.rows; i++) {
+            for(int j = 0; j < dill.cols; j++) {
                 if(shouldBeZeroImage[i][j]) {
-                    image.at<uchar>(i, j) = ZERO;
+                    dill.at<uchar>(i, j) = ZERO;
                 }
             }
-        }
+        } 
+    return dill;
 
-        string filename11="/opt/test11.txt";
- 
-        ofstream fout11(filename11);
-        for(int i = 0; i < image.rows; i++) {
-         for(int j = 0; j < image.cols; j++) {
-
-          fout11<<image.at<Vec3b>(i,j)<<",";
-             
-
-         }
-         fout11<<endl;
-       }
-        
-        fout11.close();
-
-
-        return image;
     }
