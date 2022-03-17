e746c97d1c5d86c187206d1d86406af8852f713c
Lance <53155359+Lancelof2019@users.noreply.github.com>
2022-03-17 11:36:15 +0100

    Update distanceTransform.cpp

--- functionspace/distanceTransform/distanceTransform.cpp
+++ functionspace/distanceTransform/distanceTransform.cpp
@@ -1,5 +1,16 @@
 #include "/opt/project_space/test_mnist/HPC_comb_code/MA/Watershed_Algorithm/headerspace/WatershedAlg.h"
+#include <queue>
+#include <cstdlib> 
+#include <vector>
 using namespace cv;
+
+bool comp(Point a,Point b){
+      if(a.x!=b.x){
+         return a.x<b.x;
+      }
+      return a.y<b.y;
+
+}
 
 Mat WatershedAlg::distanceTransform(Mat image, vector<Point> &markers) {
         vector< vector<double> > distanceMap(image.rows, vector<double>(image.cols, 0));
@@ -7,23 +18,11 @@ Mat WatershedAlg::distanceTransform(Mat image, vector<Point> &markers) {
         queue<int> qx, qy;
         vector<int> dx{-1, 1, 0, 0, -1, -1, 1, 1};
         vector<int> dy{0, 0, -1, 1, -1,  1, 1, -1};
-
-        string filename1="/opt/test1.txt";
-        string filename2="/opt/test2.txt";
-        string filename3="/opt/test3.txt";
-        string filename4="/opt/test4.txt";
-        string filename5="/opt/test5.txt";
-        ofstream fout1(filename1);
-        ofstream fout2(filename2);
-        ofstream fout3(filename3);
-        ofstream fout4(filename4);
-        ofstream fout5(filename5);
+        int pixelThreshold=120;
+        vector<bool>checkVisted;
+        vector<Point>zeroEdge;
         vector< vector<bool> > visited(image.rows, vector<bool>(image.cols, false));
-
-
-        
-
-
+//******It makes the nearest piexel along 0 point sohow up and become the edge pixel
         for(int i = 0; i < image.rows; i++) {
             for(int j = 0; j < image.cols; j++) {
                 
@@ -42,40 +41,31 @@ Mat WatershedAlg::distanceTransform(Mat image, vector<Point> &markers) {
 
                     if( !visited[nextX][nextY] && (int) image.at<uchar>(nextX, nextY) == ONE ) {
                         visited[nextX][nextY] = true;
-                        image.at<uchar>(nextX, nextY) = 50;
+                        image.at<uchar>(nextX, nextY) = pixelThreshold;
                         qx.push(nextX);
                         qy.push(nextY);
-                       // fout1<<(image.at<uchar>(nextX, nextY))<<",";
                        //the neighuours are all points who have 75 or 0 
                        //they are the points who are edges
+                       checkVisted.push_back(visited[nextX][nextY]);
 
                     }
                 }
+                if(checkVisted[0]||checkVisted[1]||checkVisted[2]||checkVisted[3]){
 
-                  //if(j==image.cols-1){
 
+                  zeroEdge.push_back(Point(i,j));
+                  checkVisted.clear();
+                }
 
-                  // fout1<<endl;
-
-                //  }
-
-            }
-        }
-
-      
-//
-     for(int i = 0; i < image.rows; i++) {
-         for(int j = 0; j < image.cols; j++) {
-
-          fout1<<((int)image.at<uchar>(i,j))<<",";
+         
              
 
         }
-         fout1<<endl;
-     }
         
-        fout1.close();
-//edge is equal to 50
+     }
+     //******It makes the nearest piexel along 0 point sohow up and become the edge pixel
+    
+     //edge is equal to 50
 
         while(!qx.empty()) {
             int crtX = qx.front(); qx.pop();
@@ -98,82 +88,31 @@ Mat WatershedAlg::distanceTransform(Mat image, vector<Point> &markers) {
 
                 if( (int) image.at<uchar>(crtX, crtY) +1< (int) image.at<uchar>(nextX, nextY) ) {
                     visited[nextX][nextY] = true;
-                    image.at<uchar>(nextX, nextY) = (uchar) min((image.at<uchar>(crtX, crtY)+1), 254);
-                    //cout<<"The image at crtx:"<<crtX<<"and crty:"<<crtY<<" "<<(int)image.at<uchar>(crtX, crtY)<<endl;
-                    //cout<<"The image at nextX:"<<nextX <<"and nextY:"<<nextY<<" "<<(int)image.at<uchar>(nextX, nextY)<<endl;
+                    image.at<uchar>(nextX, nextY) = (uchar) min((image.at<uchar>(crtX, crtY)+1), 254);            
                     qx.push(nextX);
                     qy.push(nextY);
-                    fout4<<"the postion of push point: "<<Point(nextX,nextY)<<"The value of image: "<<((int)image.at<uchar>(nextX,nextY))<<",";
-
-                    
-
+                  
                 }
-                fout4<<endl;
+               
             }
-           //find the max value in local area
+         
             if(isBigger) {
                 markers.push_back(Point(crtX, crtY));
-                cout<<"******************Potion and values******************"<<endl;
-                cout<<Point(crtX, crtY)<<endl;
-
-
-                cout<<(int)image.at<uchar>(crtX,crtY)<<endl;
-                //cout<<(int)image.at<uchar>(crtX,crtY)
-                cout<<"The other parts of the value in position ctrx and ctry "<<endl;
-                for(int h = 0; h < 8; h++) {
-                  int temp_nextX = crtX + dx[h];
-                  int temp_nextY = crtY + dy[h];
-                   if( temp_nextX < 0 || temp_nextY < 0 || temp_nextX >= image.rows || temp_nextY >= image.cols || (int) image.at<uchar>(temp_nextX, temp_nextY) == ZERO ) {
-                    continue;
-                }
-                  cout<<"The value of "<<"["<<temp_nextX<<","<<temp_nextY<<"]:"<<(int)image.at<uchar>(temp_nextX,temp_nextY)<<endl;
-                }
-                markerImage.at<Vec3b>(crtX, crtY) = {0, 255, 0};
-
-                //mark these compared points from last for loop
-                cout<<"******************Potion and values******************"<<endl;
-              //there will be 4 points
-
+              
             }
         
-        }
-/*
-        for(vector<Point>::iterator it=markers.begin();it!=markers.end();it++){
-         cout<<"-----The value in Point----"<<endl;
-         cout<<*it<<endl;
-         cout<<(*it).x<<endl;
-         cout<<(*it).y<<endl;
-         cout<<"--------------------------"<<endl;
-
-        }
          
-*/
-
-     for(int i = 0; i < image.rows; i++) {
-         for(int j = 0; j < image.cols; j++) {
-
-          fout3<<markerImage.at<Vec3b>(i,j)<<",";
-             
+                            
 
         }
-         fout3<<endl;
-     }
         
-        fout3.close();
 
-        
-////
-     for(int i = 0; i < image.rows; i++) {
-         for(int j = 0; j < image.cols; j++) {
 
-          fout2<<((int)image.at<uchar>(i,j))<<",";
-             
-
-        }
-         fout2<<endl;
-     }
-///////
-         fout2.close();
-        //imshow("Distance: ", image);
-        return image;
+     pixelThreshold=pixelThreshold-1;
+     int pnumThrshold=30;
+     int handlingType=0;
+     int neighbourType=0;
+     removeholesopt(image,pnumThrshold,  handlingType, neighbourType, pixelThreshold);
+	 
+     return image;
     }
