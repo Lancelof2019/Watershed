889e8807aa33b80cdd0cea01ff37ba624dc7e6b1
Lance <53155359+Lancelof2019@users.noreply.github.com>
2022-03-17 11:33:20 +0100

    Update thresholdMeasure.cpp

--- functionspace/thresholdMeasure/thresholdMeasure.cpp
+++ functionspace/thresholdMeasure/thresholdMeasure.cpp
@@ -1,27 +1,18 @@
 #include "/opt/project_space/test_mnist/HPC_comb_code/MA/Watershed_Algorithm/headerspace/WatershedAlg.h"
+#include <cmath> 
+#include <stdlib.h>
+
+
+#include <cv.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 using namespace cv;
 
 Mat WatershedAlg::thresholdMeasure(Mat image) {
-    
-
-        //threshold(image, image,ZERO,ONE, CV_THRESH_BINARY |         CV_THRESH_OTSU);
-
-        cv::adaptiveThreshold(image,image,255,cv::ADAPTIVE_THRESH_GAUSSIAN_C,cv::THRESH_BINARY,  23,2);
-        //if pixel <254 ,then 0 is given 
-       
-        string filename9="/opt/test9.txt";
-        ofstream fout9(filename9); 
-
-
-        for(int i = 0; i < image.rows; i++) {
-            for(int j = 0; j < image.cols; j++) {
-
-             fout9<<((int)image.at<uchar>(i,j))<<",";
-             
-
-         }
-          fout9<<endl;
-     }
-     fout9.close();
-        return image;
+        
+	cv::adaptiveThreshold(image,image,255,cv::ADAPTIVE_THRESH_GAUSSIAN_C,cv::THRESH_BINARY,  11,2);
+      //if pixel <254 ,then 0 is given 
+    return image;
     }
