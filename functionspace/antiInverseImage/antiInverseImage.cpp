6b50cb83ec0d4987a9fd8f327bff799c6fc350fe
Lance <53155359+Lancelof2019@users.noreply.github.com>
2022-04-18 14:47:47 +0200

    Update antiInverseImage.cpp
    adding openmp functionality

--- functionspace/antiInverseImage/antiInverseImage.cpp
+++ functionspace/antiInverseImage/antiInverseImage.cpp
@@ -3,8 +3,9 @@
 using namespace cv;
 
 Mat WatershedAlg::antiInverseImage(Mat image) {
+        #pragma omp parallel for
         for(int i = 0; i < image.rows; i++) {
-
+            #pragma omp parallel for
             for(int j = 0; j < image.cols; j++) {
                 if( (int) image.at<uchar>(i,j) == ZERO ) {
                     image.at<uchar>(i,j) = ZERO;
