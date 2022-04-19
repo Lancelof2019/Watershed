3dad6e9b1ff0050192813c8e16d637d39ec6d376
Lance <53155359+Lancelof2019@users.noreply.github.com>
2022-04-19 08:27:18 +0200

    Create WatershedAlg.h

---
+++ HPC_comb_code/MA/Watershed_Algorithm/headerspace/WatershedAlg.h
@@ -0,0 +1,53 @@
+#ifndef WATERSHEDALG_H
+#define WATERSHEDALG_H
+
+#include "/opt/project_space/test_mnist/HPC_comb_code/MA/Watershed_Algorithm/structPixel/structPixel.cpp"
+#include<iostream>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include<queue>
+#include <vector>
+#include<random>
+#include <cv.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <queue>
+#include<string>
+#include <cstdlib>
+#include<omp.h>
+using namespace cv;
+using namespace std;
+#define ZERO 0
+#define ONE 254
+#define STEP 1
+using namespace std;
+using namespace cv;
+
+class WatershedAlg{
+
+public:
+Mat makeImageGrayScale(Mat image);
+
+Mat thresholdMeasure(Mat image);
+
+Mat inverseImage(Mat image);
+
+Mat antiInverseImage(Mat image);
+
+Mat erosion(Mat image, vector< vector <int> > kernel);
+
+Mat distanceTransform(Mat image, vector<Point> &markers);
+
+Mat watershed(Mat image, vector<Point> const &markers,Mat duplImage);
+
+Mat contourFun(Mat image);
+
+void removeholesopt(Mat& src, int pnumlThrshold, int handlingType, int neighbourType,int pixelThreshhold);
+
+void processImage(Mat image,Mat duplImage) ;
+void lapseedFunc(Mat &Img);
+
+};
+#endif
