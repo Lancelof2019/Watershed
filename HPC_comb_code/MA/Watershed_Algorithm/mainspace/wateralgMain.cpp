009e925e8a72d2a54d58725a9e8c204416293147
Lance <53155359+Lancelof2019@users.noreply.github.com>
2022-05-10 14:42:21 +0200

    Update wateralgMain.cpp

--- HPC_comb_code/MA/Watershed_Algorithm/mainspace/wateralgMain.cpp
+++ HPC_comb_code/MA/Watershed_Algorithm/mainspace/wateralgMain.cpp
@@ -8,6 +8,11 @@
      clock_t start,end;
      start=clock();
      cv::Mat srcImg=cv::imread("/opt/testimages/istockphoto-1150481340-612x612.jpg");
+     if(!srcImg.data){
+      
+      cout<<"The file is not loaded or does not exist"<<endl;
+      return -1;
+     }
      cv::imshow("Test image",srcImg);
      cv::waitKey(0);
      cv::Mat duplImage(srcImg.rows,srcImg.cols,CV_8UC3,Scalar::all(0));
@@ -15,7 +20,9 @@
      WatershedAlg *walg=new WatershedAlg();
      walg->processImage(srcImg,duplImage);
      end=clock();
-     cout<<"CLOCKS_PER_SEC :"<<CLOCKS_PER_SEC<<endl;
+     delete walg;
+   
      cout<<"The total time the algorithm used is :"<<(double)(end - start)/CLOCKS_PER_SEC<<endl;
+  
      return 0;
    }
